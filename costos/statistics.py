f16448245d3830e24f104db8c09c96ebd75a16d1
rrobledo <raul.osvaldo.robledo@gmail.com>
2024-01-11 09:33:33 -0300

    adding planning api

--- costos/statistics.py
+++ costos/statistics.py
@@ -399,11 +399,12 @@ def get_planning(request):
 
     for prod in planning:
         try:
+            prod.pop("total_venta")
+            prod["precio"] = int(int(prod["precio"] if prod["precio"] is not None else 0) * 0.80)
+            prod["total_actual"] = 0
             if prod.get("product_code"):
                 ret = get_cost_by_product(request, prod.get("product_code"))
                 prod_cost = json.loads(ret.content)
-                prod.pop("total_venta")
-                prod["precio"] = round(float(prod["precio"] if prod["precio"] is not None else 0) * 0.80, 0)
                 prod["total_actual"] = prod["total_actual"] if prod["total_actual"] is not None else 0
                 prod["costo_producto"] = prod_cost.get("suggested_price")
                 prod["costo_total_planeado"] = prod.get("costo_producto") * prod.get("total")
