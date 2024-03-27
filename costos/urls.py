9863587377aa67ec5f2674f32a556b2f6bd217fc
rrobledo <raul.osvaldo.robledo@gmail.com>
2024-03-27 00:28:59 -0300

    fixing

--- costos/urls.py
+++ costos/urls.py
@@ -26,10 +26,10 @@ router.register(r'costs_details', views.CostsDetailsViewSet, basename='CostsDeta
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('reports/prices', statistics.get_all_cost, name='get_all_cost', ),
+    # path('reports/prices', statistics.get_all_cost, name='get_all_cost', ),
     path('costos_materia_prima', statistics.get_all_cost, name='get_all_cost', ),
     path('costos_materia_prima/<str:product_code>', statistics.get_cost_by_product, name='get_cost_by_product'),
-    path('products/<str:product_code>/cost', statistics.get_cost_by_product, name='get_cost_by_product'),
+    # path('products/<str:product_code>/cost', statistics.get_cost_by_product, name='get_cost_by_product'),
     path('products/<str:product_code>/history', statistics.get_product_history, name='get_product_history'),
     path('products/<str:product_code>/cronograma', statistics.get_product_cronograma, name='get_product_cronograma'),
     path('cronograma/<int:week_of_month>', statistics.get_cronograma_by_week_of_month, name='get_cronograma_by_week_of_month'),
